65a1b5f80812f0823b522f05f5207b0bd41a0645
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-08-01 15:33:45 +0530

    Update Header

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -10,14 +10,12 @@ const Header = ({ setIsNavOpen }) => {
         <div className='fixed top-0 left-0 z-10 flex items-center justify-center w-screen h-16 px-6'>
             <div className='flex items-center justify-between w-full max-w-7xl max-auto'>
                 <Link href='/'>
-                    <a className='w-32'>
-        {/*<Image src={Logo} alt={'menu'} className='w-full h-full' />*/}
-    <h1>Image Here</h1>
+                    <a>
+        <Image src={Logo} alt={'menu'} className='w-full h-full' />
                     </a>
                 </Link>
-                <div className='w-10 h-12' onClick={() => setIsNavOpen(true)}>
-                    {/*<Image src={Menu} alt={'menu'} className='w-full h-full' />*/}
-                    <h1>Menu Here</h1>
+                <div onClick={() => setIsNavOpen(true)}>
+                    <Image src={Menu} alt={'menu'} className='w-full h-full' />
                 </div>
             </div>
         </div>
