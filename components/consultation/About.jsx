45b3c2a799e135e2d5479e54d96045973ef9b0e0
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-08-01 10:40:53 +0530

    Update About Section

--- components/consultation/About.jsx
+++ components/consultation/About.jsx
@@ -4,9 +4,9 @@ import FloatingAstraunotBlack from '../../public/imgs/floatingAstraunotWhite.png
 const About = () => {
     return (
         <section className='section'>
-            {/* <div className="section-wrapper md:w-full flex justify-center lg:justify-between items-center"> */}
+            <div className="section-wrapper md:w-full flex sm:justify-center lg:justify-between items-center">
                 {/* <div className="section-wrapper md:w-full flex justify-between items-center"> */}
-                <div className="flex items-center justify-center lg:justify-between section-wrapper md:w-full">
+                {/* <div className="flex items-center justify-center lg:justify-between section-wrapper md:w-full"> */}
                 <div className='w-96 relative hidden lg:block' id='flyingAstroImg'>
                     <Image src={FloatingAstraunotBlack} alt="HeroImg" className='w-full h-full' />
                 </div>
